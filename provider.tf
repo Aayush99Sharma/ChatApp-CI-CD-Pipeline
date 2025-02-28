e932eac700e695a5ec5500ed4b871f491aa8b405
Aayush Sharma <104816668+Aayush99Sharma@users.noreply.github.com>
2025-02-28 13:48:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
-provider "aws" {
-  region = "eu-west-2"  # Change to your preferred AWS region
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+  cloud {
+
+    organization = "Aayush8276"
+
+    workspaces {
+       tags = ["project:ChatApp"]
+    }
+  }
 }
